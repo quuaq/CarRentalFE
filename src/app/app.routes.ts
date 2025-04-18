36b9399beea0acc9c28376ddb7c0575710141c5b
Samet Barış Öncül <tekerlekci145@hotmail.com>
2025-04-18 16:41:39 +0300

    Updated frontend components (Navbar, Home, About, Cars)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CarsComponent } from './pages/cars/cars.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  {path: '', component: HomeComponent}, //Ana sayfa olarak Home 
+  {path: 'about', component: AboutComponent},
+  {path: 'cars', component: CarsComponent},
 ];
