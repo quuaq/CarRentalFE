f499d6f22afde95c4e22ef464ff3d09cc6ba45eb
Samet Barış Öncül <tekerlekci145@hotmail.com>
2025-04-24 16:34:58 +0300

    car-list sayfasında araçlara fotoğraf eklendi, düzenleme yapıldı.

--- src/app/pages/car-list/car-list.component.ts
+++ src/app/pages/car-list/car-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'; 
+import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../../services/car.service';
 import { CommonModule } from '@angular/common';
 
@@ -14,9 +14,14 @@ export class CarListComponent implements OnInit {
   cars: any[] = [];
 
   constructor(
-    private route: ActivatedRoute,           
+    private route: ActivatedRoute,
     private carService: CarService
-  ) {}
+  ) { }
+
+  startDate!: Date;
+  endDate!: Date;
+  dayCount: number = 0;
+
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -24,11 +29,28 @@ export class CarListComponent implements OnInit {
       const end = params['end'];
 
       if (start && end) {
+
+        const startDate = new Date(start);
+        const endDate = new Date(end);
+
+        const dayCount = Math.ceil(
+          (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+        );
+
+
         this.carService.getAvailableCars(start, end).subscribe(data => {
-          this.cars = data;
+          this.cars = data.map(car => ({
+            ...car,
+            totalPrice: car.pricePerDay * dayCount
+          }));
           console.log(this.cars); // test amaçlı
         });
       }
+      this.startDate = new Date(start);
+      this.endDate = new Date(end);
+      this.dayCount = Math.ceil((this.endDate.getTime() - this.startDate.getTime()) / (1000 * 3600 * 24));
+
     });
+
   }
 }
